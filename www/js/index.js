8e54c3cc18293870748dd0ffcb9fd73cce6ffe23
pablo <pmunueragarcia.cf@iesesteveterradas.cat>
2023-01-13 18:05:04 +0100

    first commit

--- www/js/index.js
+++ www/js/index.js
@@ -13,11 +13,21 @@ function Hello(e){
 }
 function afegeix(){
     var text = window.prompt("Element a afegir");
-    var element = $("<li><a href='#page1'>"+text+"</a></li>");
+    var element = $("<li><a href='#page1'>"+text+"<button id='Elimina'>Elimina</button><button id='Edita'>Edita</button></a></li>");
     if(text!=null){
         $("ul").append(element);
+        $("#Elimina",element).click(elimina);
+        $("#Edita",element).click(edita);
         $("ul").listview("refresh");
     }
 }
-
-$('#Afegeix').click(afegeix);
+function elimina(e){
+    $(e.target).parent().remove();
+    return false;
+    
+}
+function edita(){
+    var text = window.prompt("Afegeix descripcio");
+    $("#page1").text(text);
+    return false;
+}
